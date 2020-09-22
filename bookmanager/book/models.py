1fd373e027fbaadddfe5b9e4bae655eb1abd2fa2
zhangbecool <zhangbecool@itcast.com>
2020-09-22 17:22:02 +0800

    迁移模型类

--- bookmanager/book/models.py
+++ bookmanager/book/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=10)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍'
+
+    def __str__(self):
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    name = models.CharField(max_length=10)
+    gender = models.BooleanField()
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = "peopleinfo"
+        verbose_name = '人物信息'
