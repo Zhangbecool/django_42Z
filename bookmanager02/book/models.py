dc0cd6395c4bfa1a0ffee28efc9a06dac73fd48f
zhangbecool <zhangbecool@itcast.com>
2020-09-23 18:54:05 +0800

    模型类

--- bookmanager02/book/models.py
+++ bookmanager02/book/models.py
@@ -2,8 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+
 class BookInfo(models.Model):
-    name = models.CharField(max_length=10)
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=10, unique=True, null=False)
+    pub_date = models.DateField(null=True, verbose_name='发布日期')
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
 
     class Meta:
         db_table = 'bookinfo'
@@ -12,17 +18,28 @@ class BookInfo(models.Model):
     def __str__(self):
         return self.name
 
+    def __repr__(self):
+        return "{},{},{},{},{},{}".format(self.id, self.name, self.pub_date, self.readcount, self.commentcount, self.is_delete)
+
+
 
 class PeopleInfo(models.Model):
+    GENDER_CHOICE = (
+        (1, 'male'),
+        (2, 'female')
+    )
     name = models.CharField(max_length=10)
-    gender = models.BooleanField()
+    gender = models.IntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, default='')
+    is_delete = models.BooleanField(default=False)
     book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
 
     class Meta:
-        db_table = 'peopleInfo'
+        db_table = 'peopleinfo'
         verbose_name = '人物信息'
 
     def __str__(self):
         return self.name
 
-
+    def __repr__(self):
+        return '[{},{},{},{},{}]'.format(self.name, self.gender, self.description, self.is_delete, self.book)
--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -8,3 +8,21 @@ def index(request):
             
             }
     return render(request, 'book/index.html', context)
+
+
+def center(request):
+    from book.models import BookInfo, PeopleInfo
+    PeopleInfo.objects.all()
+    BookInfo.objects.all()
+
+
+    # 查询编号为1的图书
+    BookInfo.objects.get(id=1)
+    # 查询书名包含'湖'的图书
+    BookInfo.objects.filter()
+    # 查询书名以'部'结尾的图书
+    # 查询书名为空的图书
+    # 查询编号为1或3或5的图书
+    # 查询编号大于3的图书
+    # 查询1980年发表的图书
+    # 查询1990年1月1日后发表的图书
