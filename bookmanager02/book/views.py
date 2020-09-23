d41176b6fe82cde389d96dc49508412fb2081676
zhangbecool <zhangbecool@itcast.com>
2020-09-23 22:40:59 +0800

    级联查询

--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -42,3 +42,41 @@ BookInfo.objects.filter(readcount__gt=F('commentcount')*2)
 BookInfo.objects.filter(Q(readcount__gt=20) | Q(id__lt=3))
 # 查询编号不等于3的图书。
 BookInfo.objects.filter(~Q(id=3))
+
+from django.db.models import Sum, Avg, Max, Min, Count
+# 查询图书的总阅读量
+BookInfo.objects.aggregate(Sum('readcount'))
+# 查询图书总数
+BookInfo.objects.count()
+
+BookInfo.objects.order_by('readcount')
+
+BookInfo.objects.order_by('-readcount')
+
+# 查询书籍为1的所有人物信息
+PeopleInfo.objects.filter(book=1)
+book = BookInfo.objects.get(id=1)
+book.peopleinfo_set()
+# 查询人物为1的书籍信息
+people = PeopleInfo.objects.get(id=1)
+people.book
+
+# 查询图书，要求图书人物为"郭靖"
+BookInfo.objects.filter(peopleinfo__name='郭靖')
+# 查询图书，要求图书中人物的描述包含"八"
+BookInfo.objects.filter(peopleinfo__description__contains='八')
+
+# 查询书名为“天龙八部”的所有人物
+BookInfo.objects.get(name='天龙八部').peopleinfo_set.all()
+PeopleInfo.objects.filter(book__name='天龙八部')
+# 查询图书阅读量大于30的所有人物
+PeopleInfo.objects.filter(book__readcount__gt=30)
+books = BookInfo.objects.filter(readcount__gt=30)
+a = [book.peopleinfo_set.all() for book in books]
+print(a)
+
+
+# 惰性
+
+books = BookInfo.objects.all()
+[book.id for book in books]
