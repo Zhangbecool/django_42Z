304cadd766fa1e8efd1a482301359f3b13d5afef
zhangbecool <zhangbecool@itcast.com>
2020-09-26 14:13:22 +0800

    练习2

--- bookmanger03/people/urls.py
+++ bookmanger03/people/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, register_converter
 from people.views import *
+from book.converters import mobile
+
+register_converter(mobile, 'phone')
 
 urlpatterns = [
-    path('<name>', name),
-    path('<name>/<height>', look),
+    path('name/', name),
+    path('<name>/<phone:mobile>', look),
+    path('eat/', eat),
+    path('register/', register),
+    path('set_cookie/', set_cookie),
+    path('get_cookie/', get_cookie),
+    path('set_session/', SetSession.as_view()),
+    path('get_session/', get_session),
 ]
--- bookmanger03/people/views.py
+++ bookmanger03/people/views.py
@@ -1,17 +1,90 @@
+import json
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views import View
 
 
-def name(request, name):
-    print(name)
+def name(request):
+    data = request.GET
+    username = data.get('username')
+    print(username)
 
-    return HttpResponse(name)
+    return HttpResponse(username)
 
 
-def look(request, name, height):
-    print(name, height)
+def look(request, name, mobile):
+    print(name, mobile)
     print(request.GET.keys())
     # request.GET.
-    return HttpResponse(name, height)
+    return HttpResponse(name, mobile)
+
+
+def eat(request):
+    data = request.POST
+    print(f'data:{data}')
+
+    return HttpResponse('post')
+
+
+def register(request):
+    data = request.body
+    print(f'data:{data}, type:{type(data)}')
+    data_str = data.decode()
+    print(f'data:{data_str}, type:{type(data_str)}')
+    data_dict = json.loads(data_str)
+    print(f'data:{data_dict}, type:{type(data_dict)}')
+    print(request.path)
+    print(request.META.get('HTTP_NAME'))
+
+    return HttpResponse('json')
+
+
+def set_cookie(request):
+    username = request.GET.get('username', max_age=60*60)
+    password = request.GET.get('password')
+
+    response = HttpResponse('set_cookie')
+    response.set_cookie('username', username)
+    response.set_cookie('password', password)
+
+    return response
+
+
+def get_cookie(request):
+    name = request.COOKIES.get('username')
+    pw = request.COOKIES.get('password')
+    # cookie = request.COOKIES
+    return HttpResponse(f'username:{name}, password:{pw}')
+
+
+class SetSession(View):
+    def post(self, request):
+        name = request.POST.get('name')
+        pw = request.POST.get('password')
+        print(name, pw)
+        request.session['name'] = name
+        request.session['password'] = pw
+
+        return HttpResponse('set_session')
+
+    def get(self, request):
+        name = request.GET.get('name')
+        pw = request.GET.get('password')
+        print(name, pw)
+        request.session['name'] = name
+        request.session['password'] = pw
+
+        return HttpResponse('set_session')
+
+
+
+def get_session(request):
+    name = request.session.get('name')
+    pw = request.session.get('password')
+    # print(request.session)
+    request.session.flush()
+    return HttpResponse(f'name:{name}, pw:{pw}')
+
